86b45435f1cadbe5390956d8a3d16a31107eef1a
Conner Biedenstein <conner.biedenstein@tsssolutions.com>
2025-11-07 13:13:27 -0500

    Code Update

---
+++ DSP/PySDR/FrequencyDomain.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+'''
+t = np.arange(100)
+s = np.sin(0.15*2*np.pi*t)
+
+#plt.plot(t, s)
+#plt.show()
+
+Sf = np.fft.fft(s)
+S_mag = np.abs(Sf)
+S_phase = np.angle(Sf)
+'''
+
+Fs = 1 #Hz
+N = 100 # Number of points to sample and the size of the FFT.
+
+t = np.arange(N) # Because the sample rate is 1 Hz
+s = np.sin(0.15*2*np.pi*t)
+s = s * np.hamming(N)
+sf = np.fft.fftshift(np.fft.fft(s))
+sf_mag = np.abs(sf)
+sf_phase = np.angle(sf)
+f = np.arange(Fs/-2, Fs/2, Fs/N)
+plt.figure(0)
+plt.plot(f, sf_mag, '.-')
+plt.grid(True)
+plt.figure(1)
+plt.plot(f, sf_phase, '.-')
+plt.grid(True)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
